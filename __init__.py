124556edbe68a0c694bd2bfe0525b1b5ab816c0b
benjamin <91140626+tpdebenjamin@users.noreply.github.com>
2024-01-25 15:14:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,8 @@ def hello_world():
     return render_template('hello.html')
 @app.route('/fr/')
 def bonjour():
-    return 'html'
+    return 'FORZA JUVENTUS'
+    return 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fespacefoot.fr%2Fmaillots-de-foot-homme%2F2812-hr8256maillotjuventusdomicile202324&psig=AOvVaw13IsmCMbN-_VM5z9De_qyq&ust=1706278408276000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDM9MTc-IMDFQAAAAAdAAAAABAD'
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
