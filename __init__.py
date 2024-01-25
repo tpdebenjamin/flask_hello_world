a78ddee020e38c42235087f64111503b5d00c210
benjamin <91140626+tpdebenjamin@users.noreply.github.com>
2024-01-25 15:12:05 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+@app.route('/fr/')
+def bonjour():
+    return 'html'
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
